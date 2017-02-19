cb876bf470144eddaa808c8df1eb31bfc7840fc0
casianosantana <santana.cms@gmail.com>
2017-02-19 17:37:19 -0300

    adicionando suporte a firebase criando paginas fundamentais

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,17 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-import { LoginPage} from '../pages/login/login.page'
-import { InputBase } from '../compartilhado/inputbase/inputBase.component'
+import { LoginPage} from '../pages/login/login.page';
+import { InputBase } from '../compartilhado/inputbase/inputBase.component';
+import {AngularFireModule} from 'angularFire2';
+
+export const fireBaseConfig = {
+    apiKey: "AIzaSyBACv__a7FtG3udDmPnTtC-cQyOFC8cLQA",
+    authDomain: "njord-22339.firebaseapp.com",
+    databaseURL: "https://njord-22339.firebaseio.com",
+    storageBucket: "njord-22339.appspot.com",
+    messagingSenderId: "282964781528"
+};
 
 @NgModule({
   declarations: [
@@ -13,7 +22,8 @@ import { InputBase } from '../compartilhado/inputbase/inputBase.component'
     InputBase
   ],
   imports: [
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(fireBaseConfig)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
